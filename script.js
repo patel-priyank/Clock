94f956d369cef47396e0aa1b5bb30e7ae4a97af3
Priyank Patel <patel23priyank@gmail.com>
2020-12-11 18:54:38 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hoursSpan = document.querySelector('#clock-hours');
+const minutesSpan = document.querySelector('#clock-minutes');
+const secondsSpan = document.querySelector('#clock-seconds');
+
+(function () {
+  var currentTime = new Date();
+
+  hoursSpan.innerHTML = currentTime.getHours().toString().padStart(2, '0');
+  minutesSpan.innerHTML = currentTime.getMinutes().toString().padStart(2, '0');
+  secondsSpan.innerHTML = currentTime.getSeconds().toString().padStart(2, '0');
+
+  setTimeout(arguments.callee, 1);
+})();
