21e8b1476f14ee86eafc8d4f7276f91d86db87c1
Priyank Patel <patel23priyank@gmail.com>
2022-03-22 04:20:02 +0530

    can now hide seconds

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const settings = document.querySelector('#settings');
 const settingsDialog = document.querySelector('#settings-dialog');
 
 const darkMode = document.querySelector('#dark-mode');
-// const showSeconds = document.querySelector('#show-seconds');
+const showSeconds = document.querySelector('#show-seconds');
 const settingsClose = document.querySelector('#settings-close');
 
 settings.addEventListener('click', () => {
@@ -59,5 +59,27 @@ function setDarkModeOff() {
   document.body.style.backgroundColor = 'hsl(0, 0%, 95%)';
   document.body.style.color = 'hsl(0, 0%, 20%)';
 }
+
+//#endregion
+
+//#region Show seconds
+
+if (localStorage.getItem('showSeconds') === 'false') {
+  showSeconds.checked = false;
+  secondsSpan.style.display = 'none';
+} else {
+  showSeconds.checked = true;
+  secondsSpan.style.display = '';
+}
+
+showSeconds.addEventListener('change', () => {
+  if (showSeconds.checked) {
+    localStorage.setItem('showSeconds', true);
+    secondsSpan.style.display = '';
+  } else {
+    localStorage.setItem('showSeconds', false);
+    secondsSpan.style.display = 'none';
+  }
+});
 
 //#endregion
