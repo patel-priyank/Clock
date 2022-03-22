cfd6df2e3d5de441bbb8c036dd422ea3a6736fd4
Priyank Patel <patel23priyank@gmail.com>
2022-03-22 05:38:55 +0530

    add support for 24-hour clock

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const hoursSpan = document.querySelector('#clock-hours');
 const minutesSpan = document.querySelector('#clock-minutes');
 const secondsSpan = document.querySelector('#clock-seconds');
+const periodSpan = document.querySelector('#clock-period');
 const settings = document.querySelector('#settings');
 const settingsDialog = document.querySelector('#settings-dialog');
 
 const darkMode = document.querySelector('#dark-mode');
 const showSeconds = document.querySelector('#show-seconds');
+const clock24Hour = document.querySelector('#clock-24-hour');
 const settingsClose = document.querySelector('#settings-close');
 
 settings.addEventListener('click', () => {
@@ -19,9 +21,21 @@ settingsClose.addEventListener('click', () => {
 (function () {
   var currentTime = new Date();
 
-  hoursSpan.innerHTML = currentTime.getHours().toString().padStart(2, '0');
+  if (localStorage.getItem('clock24Hour') === 'false') {
+    if (currentTime.getHours() === 0) {
+      hoursSpan.innerHTML = '12';
+    } else if (currentTime.getHours() > 12) {
+      hoursSpan.innerHTML = (currentTime.getHours() - 12).toString().padStart(2, '0');
+    } else {
+      hoursSpan.innerHTML = currentTime.getHours().toString().padStart(2, '0');
+    }
+  } else {
+    hoursSpan.innerHTML = currentTime.getHours().toString().padStart(2, '0');
+  }
+
   minutesSpan.innerHTML = currentTime.getMinutes().toString().padStart(2, '0');
   secondsSpan.innerHTML = currentTime.getSeconds().toString().padStart(2, '0');
+  periodSpan.innerHTML = currentTime.getHours() >= 12 ? 'PM' : 'AM';
 
   var httpRequest = new XMLHttpRequest();
   httpRequest.open('GET', '/#');
@@ -79,6 +93,28 @@ showSeconds.addEventListener('change', () => {
   } else {
     localStorage.setItem('showSeconds', false);
     secondsSpan.style.display = 'none';
+  }
+});
+
+//#endregion
+
+//#region 24-Hour Clock
+
+if (localStorage.getItem('clock24Hour') === 'false') {
+  clock24Hour.checked = false;
+  periodSpan.style.display = '';
+} else {
+  clock24Hour.checked = true;
+  periodSpan.style.display = 'none';
+}
+
+clock24Hour.addEventListener('change', () => {
+  if (clock24Hour.checked) {
+    localStorage.setItem('clock24Hour', true);
+    periodSpan.style.display = 'none';
+  } else {
+    localStorage.setItem('clock24Hour', false);
+    periodSpan.style.display = '';
   }
 });
 
